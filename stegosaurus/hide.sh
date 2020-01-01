ce47b030e52ff56c803969280e289a5083cc52cc
Greenscreener <honzikcernoh@gmail.com>
2020-01-01 17:56:36 +0100

    Created stegosaurus.

---
+++ stegosaurus/hide.sh
@@ -0,0 +1,26 @@
+printf "Pls input the message you want to \"encrypt\": "
+read message
+printf "\"Encrypting\"...\n"
+tmpDir=/tmp/stegosaurus-$RANDOM 
+mkdir $tmpDir
+cp img1.png $tmpDir
+cp img2.jpg $tmpDir
+cp img3.png $tmpDir
+cd $tmpDir
+echo $message > this-is-a-very-very-secret-message-btw
+tar -cf stage1.tar this-is-a-very-very-secret-message-btw
+cat stage1.tar | xxd -p -c1 | tac | xxd -p -r > stage2.rat
+cat img1.png <(printf "\ngrnscrnr") stage2.rat > this-is-an-image.png
+tar -cf this-is-fun-1000.tar this-is-an-image.png
+for i in {999..0}
+do
+    tar -cf this-is-fun-$i.tar this-is-fun-$(($i + 1)).tar
+    rm this-is-fun-$(($i + 1)).tar
+done
+cat img2.jpg <(printf "\ngrnscrnr") this-is-fun-0.tar > this-is-a-nice-picture-isnt-it.jpg
+tar -czf stage4.tar.gz this-is-a-nice-picture-isnt-it.jpg
+cat stage4.tar.gz | xxd -p -c1 | tac | xxd -p -r > stage5.rat.zg
+cat img3.png <(printf "\ngrnscrnr") stage5.rat.zg > this-is-such-a-lovely-picture.png
+cd - > /dev/null
+cp $tmpDir/this-is-such-a-lovely-picture.png .
+printf "$(pwd)/this-is-such-a-lovely-picture.png written. Done.\n"
---
+++ stegosaurus/unhide.sh
@@ -0,0 +1,26 @@
+filename=$1
+printf "\"Decrypting\"...\n"
+tmpDir=/tmp/stegosaurus-$RANDOM
+mkdir $tmpDir
+cp $filename $tmpDir
+cd $tmpDir
+cat $filename | awk '/grnscrnr/,0' > stage5.tmp.rat.zg
+dd if=stage5.tmp.rat.zg bs=1 skip=8 of=stage5.rat.zg 2>/dev/null
+cat stage5.rat.zg | xxd -p -c1 | tac | xxd -p -r > stage4.tmp.tar.gz
+dd if=stage4.tmp.tar.gz bs=1 skip=1 of=stage4.tar.gz 2>/dev/null
+tar -xf stage4.tar.gz
+cat this-is-a-nice-picture-isnt-it.jpg | awk '/grnscrnr/,0' > this-is-fun-0.tmp.tar
+dd if=this-is-fun-0.tmp.tar bs=1 skip=8 of=this-is-fun-0.tar 2>/dev/null
+for i in {0..1000}
+do
+    tar -xf this-is-fun-$i.tar
+    rm this-is-fun-$i.tar
+done
+cat this-is-an-image.png | awk '/grnscrnr/,0' > stage2.tmp.rat
+dd if=stage2.tmp.rat bs=1 skip=8 of=stage2.rat 2>/dev/null
+cat stage2.rat | xxd -p -c1 | tac | xxd -p -r  > stage1.tmp.tar
+dd if=stage1.tmp.tar bs=1 skip=1 of=stage1.tar 2>/dev/null
+tar -xf stage1.tar
+printf "Message: "
+cat this-is-a-very-very-secret-message-btw
+
